9e979a15121d9a93ea3b925de348360d22ddc502
vp-mx <12551270+vp-mx@users.noreply.github.com>
2025-09-08 16:53:39 +0200

    HW1

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+  },
+  base: './',
 });
